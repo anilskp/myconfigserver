412d8f5d1d6c3dadfcbc6eefae958501528e0ae8
Anil <anil.shafeeque@gmail.com>
2017-09-09 19:13:48 +0300

    First Commit

---
+++ src/main/java/com/askp/myconfigserver/MyconfigserverApplication.java
@@ -0,0 +1,14 @@
+package com.askp.myconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class MyconfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyconfigserverApplication.class, args);
+	}
+}
